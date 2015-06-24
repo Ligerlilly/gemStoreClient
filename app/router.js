c9c0dab8a4340c292ec9510f354a7008ead1bec2
Jason <quizathaderat@yahoo.com>
2015-06-24 13:43:45 -0700

    Add gems/show route

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('gems', function(){
+    this.route('show', { path: '/:gem_id'});
+  });
 });
 
 export default Router;
